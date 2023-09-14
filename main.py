c554656dd51f6bacc6a55cf68c2d0e7743e06f53
muyi2905 <obaremimuyiwa@gmail.com>
2023-09-14 11:59:27 +0000

    created choices for user and computer

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import random
 
+def game():
+    choices = ["rock", "paper", "scissors"]
+    user_choice = ""
+    
+while user_choice not in choices:
+        user_choice = input("Enter your choice between 'rock', 'paper', and 'scissors': ").lower()
